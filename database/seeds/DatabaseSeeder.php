3f5663a72b3532d5c4599877a686e1d7b680662f
yasuhiro.fujiwara <kangyang.yasuhiro@yahoo.co.jp>
2020-06-25 14:04:06 +0000

    tasklist-Procfile

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('messages')->insert([
+            'title' => 'test title 1',
+            'content' => 'test content 1'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 2',
+            'content' => 'test content 2'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 3',
+            'content' => 'test content 3'
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
