0ba1b696a4be6647c706e200fa03a969c5ff2942
yasuhiro.fujiwara <kangyang.yasuhiro@yahoo.co.jp>
2020-07-03 03:42:54 +0000

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 
     <h1>タスク一覧</h1>
+    {{-- ユーザ登録ページへのリンク --}}
+    {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
